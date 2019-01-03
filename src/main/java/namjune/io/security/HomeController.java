60002c6b7b4abaa00e77606b0c1c809b87198c41
Namjun Kim <namjunemy@gmail.com>
2019-01-03 22:54:38 +0900

    뷰페이지, 컨트롤러, 컨트롤러 테스트 코드 작성

---
+++ src/main/java/namjune/io/security/HomeController.java
@@ -0,0 +1,23 @@
+package namjune.io.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/my")
+    public String my() {
+        return "my";
+    }
+}
